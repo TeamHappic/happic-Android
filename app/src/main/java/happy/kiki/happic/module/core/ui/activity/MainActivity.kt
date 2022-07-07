749c2a28fdd1a3a1a92738cdeb9e95a09fd81e74
mym0404 <mym0404@gmail.com>
2022-07-08 01:56:23 +0900

    [refactor] Move MainActivity.kt

---
+++ app/src/main/java/happy/kiki/happic/module/core/ui/activity/MainActivity.kt
@@ -0,0 +1,14 @@
+package happy.kiki.happic.module.core.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import happy.kiki.happic.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+    }
+}
