be0f3c3d51e009c90345b18529187b1918e13f7c
mym0404 <mym0404@gmail.com>
2022-07-10 01:42:37 +0900

    [feat] Create Activity/Fragment navigation utility extension functions

---
+++ app/src/main/java/happy/kiki/happic/module/core/util/extension/ActivityNavigationExtension.kt
@@ -0,0 +1,62 @@
+package happy.kiki.happic.module.core.util.extension
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Parcelable
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+const val _ACTIVITY_ARGUMENT_KEY_ = "_ACTIVITY_ARGUMENT_KEY_"
+
+inline fun <reified T : Activity> Activity.pushActivity(
+    intentConfig: (Intent) -> Intent = { it },
+) = startActivity(intentConfig(Intent(this, T::class.java)))
+
+inline fun <reified T : Activity> Activity.pushActivity(
+    arg: Parcelable,
+    intentConfig: (Intent) -> Intent = { it },
+) = pushActivity<T> {
+    it.putExtra(_ACTIVITY_ARGUMENT_KEY_, arg)
+    it.let(intentConfig)
+}
+
+inline fun <reified T : Fragment> AppCompatActivity.addFragment(
+    container: ViewGroup,
+    arg: Parcelable? = null,
+    tag: String? = null,
+    skipAddToBackStack: Boolean = false,
+    backStackEntryName: String? = null
+) = supportFragmentManager.commit {
+    add(container.id, T::class.java, bundleOf(_FRAGMENT_ARGUMENT_KEY_ to arg), tag)
+
+    if (!skipAddToBackStack) addToBackStack(backStackEntryName)
+}
+
+inline fun <reified T : Fragment> AppCompatActivity.replaceFragment(
+    container: ViewGroup,
+    arg: Parcelable? = null,
+    tag: String? = null,
+    skipAddToBackStack: Boolean = false,
+    backStackEntryName: String? = null
+) = supportFragmentManager.commit {
+    replace<T>(container.id, tag, bundleOf(_FRAGMENT_ARGUMENT_KEY_ to arg))
+
+    if (!skipAddToBackStack) addToBackStack(backStackEntryName)
+}
+
+fun AppCompatActivity.popBackStack() = supportFragmentManager.popBackStack()
+fun AppCompatActivity.popBackStacksUntilNameFound(name: String) = supportFragmentManager.popBackStack(name, 0)
+
+class ActivityArgumentDelegate<Arg : Parcelable> : ReadOnlyProperty<AppCompatActivity, Arg> {
+    override fun getValue(thisRef: AppCompatActivity, property: KProperty<*>) =
+        thisRef.intent?.getParcelableExtra<Arg>(_ACTIVITY_ARGUMENT_KEY_)
+            ?: throw RuntimeException("${this::class.java.simpleName}이 argument를 가져오는 데 실패했습니다")
+}
+
+fun <Arg : Parcelable> AppCompatActivity.argument() = ActivityArgumentDelegate<Arg>()
---
+++ app/src/main/java/happy/kiki/happic/module/core/util/extension/FragmentNavigationExtension.kt
@@ -0,0 +1,55 @@
+package happy.kiki.happic.module.core.util.extension
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Parcelable
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+const val _FRAGMENT_ARGUMENT_KEY_ = "_FRAGMENT_ARGUMENT_KEY_"
+
+inline fun <reified T : Activity> Fragment.pushActivity(
+    intentConfig: (Intent) -> Intent = { it },
+) = requireActivity().pushActivity<T>(intentConfig)
+
+inline fun <reified T : Activity> Fragment.pushActivity(
+    arg: Parcelable,
+    intentConfig: (Intent) -> Intent = { it },
+) = requireActivity().pushActivity<T>(arg, intentConfig)
+
+inline fun <reified T : Fragment> Fragment.addFragment(
+    container: ViewGroup,
+    arg: Parcelable? = null,
+    tag: String? = null,
+    skipAddToBackStack: Boolean = false,
+    backStackEntryName: String? = null
+) = parentFragmentManager.commit {
+    add(container.id, T::class.java, bundleOf(_FRAGMENT_ARGUMENT_KEY_ to arg), tag)
+
+    if (!skipAddToBackStack) addToBackStack(backStackEntryName)
+}
+
+inline fun <reified T : Fragment> Fragment.replaceFragment(
+    container: ViewGroup,
+    arg: Parcelable? = null,
+    tag: String? = null,
+    skipAddToBackStack: Boolean = false,
+    backStackEntryName: String? = null
+) = parentFragmentManager.commit {
+    replace<T>(container.id, tag, bundleOf(_FRAGMENT_ARGUMENT_KEY_ to arg))
+
+    if (!skipAddToBackStack) addToBackStack(backStackEntryName)
+}
+
+class FragmentArgumentDelegate<Arg : Parcelable> : ReadOnlyProperty<Fragment, Arg> {
+    override fun getValue(thisRef: Fragment, property: KProperty<*>) =
+        thisRef.arguments?.getParcelable<Arg>(_FRAGMENT_ARGUMENT_KEY_)
+            ?: throw RuntimeException("${this::class.java.simpleName}이 argument를 가져오는 데 실패했습니다")
+}
+
+fun <Arg : Parcelable> Fragment.argument() = FragmentArgumentDelegate<Arg>()
