9ee74f5f9b2cba34a4dab0cb2806dca136942df5
mym0404 <mym0404@gmail.com>
2022-07-10 03:16:10 +0900

    [feat] Set SignInActivity system ui layout

--- app/src/main/java/happy/kiki/happic/module/auth/ui/activity/SignInActivity.kt
+++ app/src/main/java/happy/kiki/happic/module/auth/ui/activity/SignInActivity.kt
@@ -3,15 +3,20 @@ package happy.kiki.happic.module.auth.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import happy.kiki.happic.databinding.ActivitySignInBinding
-import happy.kiki.happic.module.core.util.debugE
+import happy.kiki.happic.module.core.util.extension.windowHandler
 
 class SignInActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignInBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ActivitySignInBinding.inflate(layoutInflater).also { binding = it;setContentView(it.root) }
+        ActivitySignInBinding.inflate(layoutInflater).also {
+            binding = it;setContentView(it.root)
+        }
 
-        fun add(a: Int, b: Int) = a + b
+        windowHandler.run {
+            setStatusBarTransparent()
+            allowViewOverlapWithStatusBar()
+        }
     }
 }
