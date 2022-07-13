91f58f62b5b02972a3507014f107d7f2513d932c
mym0404 <mym0404@gmail.com>
2022-07-13 13:36:43 +0900

    [feat] LayoutAnimation to SignInActivity.kt

--- app/src/main/java/happy/kiki/happic/module/auth/ui/activity/SignInActivity.kt
+++ app/src/main/java/happy/kiki/happic/module/auth/ui/activity/SignInActivity.kt
@@ -1,7 +1,14 @@
 package happy.kiki.happic.module.auth.ui.activity
 
 import android.os.Bundle
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.buildSpannedString
+import androidx.core.text.underline
+import happy.kiki.happic.R
 import happy.kiki.happic.databinding.ActivitySignInBinding
 import happy.kiki.happic.module.core.util.debugE
 import happy.kiki.happic.module.core.util.extension.windowHandler
@@ -18,6 +25,32 @@ class SignInActivity : AppCompatActivity() {
         windowHandler.run {
             setStatusBarTransparent()
             allowViewOverlapWithStatusBar()
+        }
+
+        // FIXME clickable implementation
+        binding.bottomText.isClickable = true
+        binding.bottomText.movementMethod = LinkMovementMethod.getInstance()
+        binding.bottomText.text = buildSpannedString {
+            append("로그인 시 ")
+
+            underline {
+                append("이용약관", object : ClickableSpan() {
+                    override fun onClick(p0: View) {
+                        debugE("123")
+                    }
+
+                    override fun updateDrawState(ds: TextPaint) {
+                        super.updateDrawState(ds)
+                        ds.isUnderlineText = false
+                        ds.color = getColor(R.color.gray6)
+                    }
+                }, 0)
+            }
+            append("과 ")
+            underline {
+                append("개인정보 처리 방침")
+            }
+            append("에 동의하게 됩니다.")
         }
     }
 }
