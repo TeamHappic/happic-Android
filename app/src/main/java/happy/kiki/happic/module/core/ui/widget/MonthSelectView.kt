cc8ad367022c98d190e9130fc7a96a6278258e99
xxeol2 <jhj01177@naver.com>
2022-07-13 15:30:55 +0900

    [feat] monthselect view

---
+++ app/src/main/java/happy/kiki/happic/module/core/ui/widget/MonthSelectView.kt
@@ -0,0 +1,75 @@
+package happy.kiki.happic.module.core.ui.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import android.widget.TextView
+import androidx.core.view.children
+import happy.kiki.happic.R
+import happy.kiki.happic.databinding.ViewMonthSelectBinding
+import happy.kiki.happic.module.core.util.extension.getColor
+import java.time.LocalDate
+
+class MonthSelectView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) :
+    FrameLayout(context, attrs) {
+    private val monthSelectView by lazy {
+        ViewMonthSelectBinding.inflate(LayoutInflater.from(context))
+    }
+
+    init {
+        addView(monthSelectView.root)
+        monthSelectView.ivArrowPrevious.setOnClickListener { onClickLeftArrow?.invoke() }
+        monthSelectView.ivArrowNext.setOnClickListener { onClickRightArrow?.invoke() }
+
+        updateTextUiStates()
+        setMonthTextViewClickListeners()
+    }
+
+    private var year = LocalDate.now().year
+    private var selectedMonth = LocalDate.now().monthValue
+    private val isCurrentYear get() = year == LocalDate.now().year
+
+    var onClickLeftArrow: (() -> Unit)? = null
+    var onClickRightArrow: (() -> Unit)? = null
+    var onClickMonthText: ((Int) -> Unit)? = null
+
+    fun setYear(year: Int) {
+        this.year = year
+        this.monthSelectView.tvYear.text = year.toString()
+        updateTextUiStates()
+    }
+
+    fun setMonthSelected(month: Int) {
+        selectedMonth = month
+        updateTextUiStates()
+    }
+
+    private val monthTextViews: Sequence<TextView>
+        get() = monthSelectView.clMonth.children.filter { it is TextView }.map { it as TextView }
+
+    private fun setMonthTextViewClickListeners() = monthTextViews.forEachIndexed { index, view ->
+        val monthIdx = index + 1
+        view.setOnClickListener {
+            onClickMonthText?.invoke(monthIdx)
+        }
+    }
+
+    private fun updateTextUiStates() = monthTextViews.forEachIndexed { index, view ->
+        val monthIdx = index + 1
+        view.run {
+            setTextAppearance(
+                if (selectedMonth == monthIdx) R.style.Bold_16
+                else R.style.Medium_16
+            )
+            setTextColor(
+                getColor(
+                    if (selectedMonth == monthIdx) R.color.dark_purple
+                    else if (isCurrentYear && monthIdx > LocalDate.now().month.value) R.color.gray7
+                    else R.color.white
+                )
+            )
+        }
+    }
+
+}
--- app/src/main/java/happy/kiki/happic/module/home/ui/fragment/DailyHappicPhotoFragment.kt
+++ app/src/main/java/happy/kiki/happic/module/home/ui/fragment/DailyHappicPhotoFragment.kt
@@ -1,22 +1,21 @@
 import android.os.Bundle
-import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
-import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import happy.kiki.happic.R
 import happy.kiki.happic.databinding.FragmentDailyHappicPhotoBinding
 import happy.kiki.happic.databinding.ItemDailyHappicPhotoBinding
 import happy.kiki.happic.module.core.util.AutoCleardValue
-import happy.kiki.happic.module.core.util.extension.px
-import happy.kiki.happic.module.core.util.extension.screenWidth
+import happy.kiki.happic.module.core.util.extension.collectFlowWhenStarted
 import happy.kiki.happic.module.core.util.extension.fadeIn
 import happy.kiki.happic.module.core.util.extension.fadeOut
+import happy.kiki.happic.module.core.util.extension.px
+import happy.kiki.happic.module.core.util.extension.screenWidth
+import kotlinx.coroutines.flow.MutableStateFlow
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
@@ -28,50 +27,48 @@ class DailyHappicPhotoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         initData()
-        setCurrentMonths()
         setCards()
-        setOnClickListener()
+        configureMonthSelect()
     }
 
     private fun initData() {
         with(binding) {
             val now = LocalDate.now()
-            tvMonth.text = now.format(DateTimeFormatter.ofPattern("yyyy.MM")).toString()
-            monthSelectContainer.tvYear.text = now.year.toString()
+            tvMonth.text = now.format(DateTimeFormatter.ofPattern("yyyy.MM"))
+                .toString() //            monthSelectContainer.tvYear.text = now.year.toString()
         }
     }
 
-    private fun setOnClickListener() {
-        binding.borderMonth.setOnClickListener {
-            with(binding.monthSelectContainer.monthSelection) {
-                if (this.isVisible) this.fadeOut()
-                else this.fadeIn()
-            }
-        }
+    private val year = MutableStateFlow(LocalDate.now().year)
+    private val selectedMonth = MutableStateFlow(LocalDate.now().monthValue)
 
-        with(binding.monthSelectContainer) {
-            ivArrowPrevious.setOnClickListener {
-                val year = tvYear.text.toString().toInt() - 1
-                setMonthTransactionEvent(year)
-            }
-            ivArrowNext.setOnClickListener {
-                val year = tvYear.text.toString().toInt() + 1
-                setMonthTransactionEvent(year)
-            }
-        }
+    private fun configureMonthSelect() {
 
-    }
+        binding.monthSelect.apply {
+            binding.borderMonth.setOnClickListener {
+                with(this) {
+                    if (this.isVisible) this.fadeOut()
+                    else this.fadeIn()
+                }
+            }
+            
+            onClickRightArrow = {
+                val currentYear = LocalDate.now().year
+                if (year.value < currentYear) year.value += 1
+            }
+            onClickLeftArrow = {
+                year.value -= 1
+            }
 
-    private fun setMonthTransactionEvent(year: Int) {
-        with(binding.monthSelectContainer) {
-            tvYear.text = year.toString()
-            val isNotCurrentYear = year < LocalDate.now().year
-            ivArrowNext.isVisible = isNotCurrentYear
-            clMonth.removeAllViews() //            setCurrentMonths()
-            setPastMonths() //            flowMonth.allViews.forEach {
-            //                flowMonth.removeView(it)
-            //            }
-            //            if (isNotCurrentYear) setPastMonths() else setCurrentMonths()
+            collectFlowWhenStarted(selectedMonth) {
+                setMonthSelected(it)
+            }
+            collectFlowWhenStarted(year) {
+                setYear(it)
+            }
+            onClickMonthText = {
+                binding.monthSelect.setMonthSelected(it)
+            }
         }
     }
 
@@ -86,52 +83,5 @@ class DailyHappicPhotoFragment : Fragment() {
             binding.flowCards.addView(itemBinding.root)
         }
     }
-
-    private fun setCurrentMonths() {
-        binding.monthSelectContainer.ivArrowNext.isVisible = false
-        val month = LocalDate.now().month.value
-        var textStyle = R.style.Medium_16
-        var isTextClickable = true
-        var textColor = R.color.gray2
-        (0..11).map {
-            if (it + 1 == month) {
-                textStyle = R.style.Bold_16
-                textColor = R.color.dark_purple
-            } else if (it + 1 > month) {
-                textStyle = R.style.Medium_16
-                isTextClickable = false
-                textColor = R.color.gray7
-            }
-
-            val textView = TextView(ContextThemeWrapper(context, textStyle)).apply {
-                isClickable = isTextClickable
-                id = ViewCompat.generateViewId()
-                includeFontPadding = true
-                "${it + 1}월".also { text = it }
-                setTextColor(context.getColor(textColor))
-            }
-            with(binding.monthSelectContainer) {
-                clMonth.addView(textView, ConstraintLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT))
-                flowMonth.addView(textView)
-            }
-        }
-    }
-
-    private fun setPastMonths() {
-        (0..11).map {
-            val textView = TextView(ContextThemeWrapper(context, R.style.Medium_16)).apply {
-                isClickable = true
-                id = ViewCompat.generateViewId()
-                includeFontPadding = true
-                "${it + 1}월".also { text = it }
-                setTextColor(context.getColor(R.color.gray2))
-            }
-            with(binding.monthSelectContainer) {
-                clMonth.addView(textView, ConstraintLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT))
-                flowMonth.addView(textView)
-            }
-        }
-    }
-
 }
 
