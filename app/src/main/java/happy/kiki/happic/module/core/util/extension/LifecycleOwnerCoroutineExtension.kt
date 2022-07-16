c77e7ff6f6e386da049e3ca767df71d1d1de571a
mym0404 <mym0404@gmail.com>
2022-07-16 12:47:42 +0900

    [chore] Create collectFlow in ViewModel

--- app/src/main/java/happy/kiki/happic/module/core/util/extension/LifecycleOwnerCoroutineExtension.kt
+++ app/src/main/java/happy/kiki/happic/module/core/util/extension/LifecycleOwnerCoroutineExtension.kt
@@ -3,8 +3,10 @@ package happy.kiki.happic.module.core.util.extension
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 
@@ -35,3 +37,11 @@ inline fun <reified T> Fragment.collectFlowWhen(
 inline fun <reified T> Fragment.collectFlowWhenStarted(
     flow: Flow<T>, crossinline block: suspend (T) -> Unit
 ) = collectFlowWhen(flow, Lifecycle.State.STARTED, block)
+
+inline fun <reified T> ViewModel.collectFlow(
+    flow: Flow<T>, crossinline block: suspend (T) -> Unit
+) = viewModelScope.launch {
+    flow.collect {
+        block(it)
+    }
+}
