9281f55e3f7f4102f6477887bdcde5d6bfdee973
mym0404 <mym0404@gmail.com>
2022-07-16 12:47:53 +0900

    [chore] TestService

--- app/src/main/java/happy/kiki/happic/module/core/data/api/TestService.kt
+++ app/src/main/java/happy/kiki/happic/module/core/data/api/TestService.kt
@@ -1,11 +1,24 @@
 package happy.kiki.happic.module.core.data.api
 
 import happy.kiki.happic.module.core.data.api.base.ApiResponse
+import happy.kiki.happic.module.core.data.api.base.NoDataApiResponse
 import kotlinx.coroutines.delay
+import kotlin.math.roundToLong
 
 object TestService {
-    suspend fun getList(): ApiResponse<List<Int>> {
-        delay(1000)
-        return ApiResponse(200, "", listOf(1, 2, 3))
+    var a = 1
+    suspend fun itemWithoutParameter(): ApiResponse<List<Int>> {
+        delay((Math.random() * 5000 + 1000).roundToLong())
+        a += 1
+        return ApiResponse(200, "", List(a) { it })
+    }
+
+    suspend fun itemWithParameter(params: Int): ApiResponse<List<Int>> {
+        delay((Math.random() * 5000 + 1000).roundToLong())
+        return ApiResponse(200, "", List(params) { it })
+    }
+
+    suspend fun nullReturn(): NoDataApiResponse {
+        return ApiResponse(200, "", null)
     }
 }
