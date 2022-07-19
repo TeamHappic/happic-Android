a1b1cae1ff435dd3131a9d6930515c9daf2bae70
xxeol2 <jhj01177@naver.com>
2022-07-19 22:01:33 +0900

    [feat] dailyHappic ViewModel 분리

---
+++ app/src/main/java/happy/kiki/happic/module/dailyhappic/ui/fragment/DailyHappicViewModel.kt
@@ -0,0 +1,21 @@
+package happy.kiki.happic.module.dailyhappic.ui.fragment
+
+import androidx.lifecycle.ViewModel
+import happy.kiki.happic.module.core.data.api.base.useApi
+import happy.kiki.happic.module.core.util.now
+import happy.kiki.happic.module.dailyhappic.data.api.dailyHappicKeywordMockService
+import happy.kiki.happic.module.dailyhappic.data.model.DailyHappicPhotoListModel
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class DailyHappicViewModel : ViewModel() {
+    val dailyHappicPhotosApi = useApi<Pair<Int, Int>, List<DailyHappicPhotoListModel>> { (year, month) ->
+        dailyHappicKeywordMockService.photos(year, month)
+    }
+
+    val currentYear = MutableStateFlow(now.year)
+    val selectedYearMonth = MutableStateFlow(now.year to now.monthValue)
+
+    init {
+        dailyHappicPhotosApi.call()
+    }
+}
