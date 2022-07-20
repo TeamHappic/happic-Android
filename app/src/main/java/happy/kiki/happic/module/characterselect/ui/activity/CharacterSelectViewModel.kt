55de91d02a0446649cae0e7e093ffb5a926086b5
kimdahee7 <kikidahee7@naver.com>
2022-07-20 16:01:24 +0900

    [feat] mutableStateFlow Viewmodel로 분리

---
+++ app/src/main/java/happy/kiki/happic/module/characterselect/ui/activity/CharacterSelectViewModel.kt
@@ -0,0 +1,20 @@
+package happy.kiki.happic.module.characterselect.ui.activity
+
+import androidx.lifecycle.ViewModel
+import happy.kiki.happic.module.characterselect.data.api.CharacterService.RegisterCharacterNameReq
+import happy.kiki.happic.module.characterselect.data.api.characterService
+import happy.kiki.happic.module.characterselect.data.enumerate.CharacterType
+import happy.kiki.happic.module.core.data.api.base.useApi
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class CharacterSelectViewModel : ViewModel() {
+    val characterName = MutableStateFlow("")
+    val characterType = MutableStateFlow(CharacterType.MOON)
+    val characterQueryApi = useApi<RegisterCharacterNameReq> {
+        characterService.registerCharacterName(it)
+    }
+
+    init {
+        characterQueryApi.call(RegisterCharacterNameReq(characterType.value, characterName.value))
+    }
+}
