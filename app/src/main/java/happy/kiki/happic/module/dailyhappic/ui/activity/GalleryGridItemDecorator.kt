62df0c825bc13c4e48e4b3212cdc558522c3ebf4
mym0404 <mym0404@gmail.com>
2022-07-22 02:24:21 +0900

    Upload

--- app/src/main/java/happy/kiki/happic/module/dailyhappic/ui/activity/GalleryGridItemDecorator.kt
+++ app/src/main/java/happy/kiki/happic/module/dailyhappic/ui/activity/GalleryGridItemDecorator.kt
@@ -5,9 +5,9 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
 class GalleryGridItemDecorator(private val space: Int) : RecyclerView.ItemDecoration() {
-    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
-        val pos = parent.getChildLayoutPosition(view)
-        outRect.right = if (pos % 3 == 0) 0 else space
-        outRect.bottom = space
-    }
+//    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+//        val pos = parent.getChildLayoutPosition(view)
+//        outRect.right = if (pos % 3 == 0) 0 else space
+//        outRect.bottom = space
+//    }
 }
